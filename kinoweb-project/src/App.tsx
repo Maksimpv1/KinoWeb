3bdc78e9f4cbdacc267381d93a024914c269005c
Maksim noyt <maksimpv1@bk.ru>
2023-10-18 17:50:10 +0300

    start projecr

---
+++ kinoweb-project/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Footer } from "./components/ui/footer";
+import { Header } from "./components/ui/header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
